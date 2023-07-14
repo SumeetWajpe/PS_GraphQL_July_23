dcc994b0eb4f9dcef820c2c3ae2775fe8eb77d91
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-14 18:26:52 +0530

    ✨ Adding Apollo Server (instance)

---
+++ DAY-I/HelloGraphQL/src/index.ts
@@ -0,0 +1,11 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
+
+console.log(`🚀 Server ready at ${url}`);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PS_GraphQL_July_23
+
+https://github.com/SumeetWajpe/PS_GraphQL_July_23
