8a59f1d68efb9214f5aae65fd17f49e312aca9b3
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-15 14:27:51 +0530

    ✨ Using Apollo Server as Express Middleware

---
+++ DAY-II/onlineshopping-app-apollo-server-mongodb/dist/schema/typeDefs.js
@@ -0,0 +1,25 @@
+export const typeDefs = `#graphql
+
+#root-query
+type Query{
+    books:[Book]
+    book(id:ID!):Book
+    authors:[Author]
+    author(id:ID!):Author
+}
+
+type Book{
+  id:ID!
+  title:String
+  epub:Boolean
+  author:Author
+  publication:String
+}
+type Author{
+  id:ID!
+  name:String
+  age:Int
+  noOfBooks:Int
+}
+
+`;
---
+++ DAY-II/onlineshopping-app-apollo-server-mongodb/src/schema/resolvers.ts
@@ -0,0 +1,5 @@
+import { AuthorModel, authorsData } from "../models/authors.model.js";
+import { BookModel, booksData } from "../models/books.model.js";
+export const resolvers = {
+  Query: {},
+};
