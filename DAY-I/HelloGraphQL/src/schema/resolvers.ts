d1556b670b4b68948925ab024c23998a152715c5
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-15 12:18:11 +0530

    ✨ Aliases And Fragments

--- DAY-I/HelloGraphQL/src/schema/resolvers.ts
+++ DAY-I/HelloGraphQL/src/schema/resolvers.ts
@@ -3,8 +3,10 @@ import { BookModel, booksData } from "../models/books.model.js";
 export const resolvers = {
   Query: {
     books: (): BookModel[] => booksData.books,
-    book: (_, { id }): BookModel => booksData.books.find(b => b.id === +id),
+    book: (_, { id }: { id: number }): BookModel =>
+      booksData.books.find(b => b.id === +id),
     authors: (): Author[] => authorsData.authors,
-    author: (_, { id }): Author => authorsData.authors.find(a => a.id === +id),
+    author: (_, { id }: { id: number }): Author =>
+      authorsData.authors.find(a => a.id === +id),
   },
 };
